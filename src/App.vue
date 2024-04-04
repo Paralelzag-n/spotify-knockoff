40a890038be6a85292c08287e582377bc7447d7d
Giorgi Tarkhnishvili <giorgi.tarkhnishvili@lavapi.com>
2024-04-04 22:45:39 +0400

    feat: work on base dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <main class="min-h-screen">
     <RouterView />
+    <div
+      id="fixed-components"
+      class="fixed top-0 left-0 w-screen h-screen bg-sky-500-500/10 z-50 pointer-events-none"
+    />
   </main>
 </template>
